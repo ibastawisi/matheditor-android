83549b7e9d7b7cc0905cd566d24f50feedeee59d
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-07-13 00:24:10 +0300

    splash screen color styles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "ml.matheditor.twa"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
+        versionCode = 10
         versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
